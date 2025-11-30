a9f977af2136db31a3758220cbfc6cbb59b00be7
Арина Костовская <kostovar1601@gmail.com>
2025-11-30 22:26:35 +0500

    Задание 9.1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { generatePhotosArray } from './data.js';
 import { renderThumbnails, initThumbnailsHandlers } from './pictures.js';
+import { initForm } from './form.js';
 
 const photosArray = generatePhotosArray();
 
@@ -18,5 +19,9 @@ const initApp = () => {
   initThumbnailsHandlers();
 };
 
+// Инициализация формы при загрузке страницы
+document.addEventListener('DOMContentLoaded', () => {
+  initForm();
+});
 // Запускаем приложение после загрузки DOM
 document.addEventListener('DOMContentLoaded', initApp);
